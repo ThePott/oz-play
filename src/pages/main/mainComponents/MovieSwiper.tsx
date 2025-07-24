688a623b3296742abd206ac1833093be9382b452
Tynahan <nusilite@gmail.com>
2025-07-25 06:41:02 +0900

    ✨ Feat(swiper): Add basic swiper with swiper lib

--- src/pages/main/mainComponents/MovieSwiper.tsx
+++ src/pages/main/mainComponents/MovieSwiper.tsx
@@ -1,15 +1,69 @@
-import { Box } from '@mui/material'
-import type { MovieCardInfo } from '../../../_interfaces/interfaces'
-import MovieCardHorizontal from './MovieCardHorizontal'
+import { useRef } from 'react';
+import { Swiper as SwiperType } from "swiper";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
+import type { MovieCardInfo } from '../../../_interfaces/interfaces';
+import MovieCardHorizontal from './MovieCardHorizontal';
+import { Box, IconButton } from '@mui/material';
+import ArrowBackIosNewRoundedIcon from '@mui/icons-material/ArrowBackIosNewRounded';
+import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
+
+const sxBase = {
+  position: "absolute",
+  zIndex: 10,
+  top: "50%",
+  transform: "translateY(-50%)",
+  color: "oklch(0 0 0)",
+}
+
+const PrevButton = ({ swiperRef }: { swiperRef: React.RefObject<SwiperType | null> }) => {
+  return (
+    <IconButton aria-label="previous"
+      sx={{...sxBase, left: 0}}
+      onClick={() => swiperRef.current?.slidePrev()}>
+
+      <ArrowBackIosNewRoundedIcon />
+
+    </IconButton>
+  );
+};
+
+const NextButton = ({ swiperRef }: { swiperRef: React.RefObject<SwiperType | null> }) => {
+  return (
+    <IconButton aria-label="previous"
+      sx={{...sxBase, right: 0}}
+      onClick={() => swiperRef.current?.slideNext()}>
+
+      <ArrowForwardIosRoundedIcon />
+
+    </IconButton>
+  );
+};
 
 const MovieSwiper = ({ movieCardInfoArray }: { movieCardInfoArray: MovieCardInfo[] }) => {
+  const swiperRef = useRef<SwiperType | null>(null);
+
+
   return (
-    <Box sx={{ scrollbarWidth: "none" }} className="flex gap-12 overflow-scroll py-12">
-      {movieCardInfoArray.map((movieCardInfo, index) => (
-        <MovieCardHorizontal key={index} movieCardInfo={movieCardInfo} />
-      ))}
+    <Box className='relative'>
+      <Swiper
+        spaceBetween={200}
+        slidesPerView={3}
+        pagination={{ clickable: true }}
+        onSwiper={(swiper: any) => { swiperRef.current = swiper }}>
+
+        {movieCardInfoArray.map((movieCardInfo, index) => (
+          <SwiperSlide>
+            <MovieCardHorizontal key={index} movieCardInfo={movieCardInfo} />
+          </SwiperSlide>
+        ))}
+
+      </Swiper>
+
+      <PrevButton swiperRef={swiperRef} />
+      <NextButton swiperRef={swiperRef} />
     </Box>
-  )
-}
+  );
+};
 
 export default MovieSwiper
--- src/pages/test/TestPage.tsx
+++ src/pages/test/TestPage.tsx
@@ -1,43 +1,26 @@
-import { Box, Typography } from '@mui/material';
 import { Swiper, SwiperSlide } from 'swiper/react';
-// import 'swiper/components/navigation/navigation.min.css';
-// import 'swiper/swiper.min.css';
+import 'swiper/css';
 
-// import Swiper core and required modules
-import SwiperCore from 'swiper/core';
-import { Navigation } from 'swiper/modules';
-
-// install Swiper modules
-SwiperCore.use([Navigation]);
-
-const TestPage = () => {
+export default () => {
   return (
-    <Box className="App" padding={2}>
-      <Typography
-        variant={'h4'}
-        align={'center'}
-        fontWeight={700}
-      >
-        Swiper + Material-UI example
-      </Typography>
-      {/** Slider main container */}
-      <Box marginTop={4}>
-        <Swiper navigation={true} className="mySwiper">
-          {/** Slides */}
-          {['Slide 1', 'Slide 2', 'Slide 3'].map((item, i) => (
-            <SwiperSlide key={i}>
-              <Typography
-                variant={'h6'}
-                align={'center'}
-              >
-                {item}
-              </Typography>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </Box>
-    </Box>
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+    </Swiper>
   );
-}
-
-export default TestPage
+};
