df2cf23d24ac84e1f02636f02f27cf66a3ed5aab
Tynahan <nusilite@gmail.com>
2025-07-25 08:31:04 +0900

    💄 Style(swiper): Show swiper slides properly

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -8,11 +8,13 @@ const MainPage = () => {
   const movieCardInfoArray: MovieCardInfo[] = movieListData.results
 
   const hotMovieCardInfoArray = movieCardInfoArray.filter((movieCardInfo) => movieCardInfo.vote_average >= 7)
+  hotMovieCardInfoArray.sort((a, b) => b.vote_average - a.vote_average)
 
 
 
   return (
-    <Box sx={{ scrollbarColor: "oklch(0.5 0 0) transparent" }} className="h-full overflow-scroll">
+    <Box sx={{ scrollbarColor: "oklch(0.5 0 0) transparent" }} 
+    className="h-full overflow-scroll">
       <MovieSwiper movieCardInfoArray={hotMovieCardInfoArray} />
       <Box className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-3 ">
         {movieCardInfoArray.map((movieCardInfo, index) => <MovieCard key={index} movieCardInfo={movieCardInfo} />)}
--- src/pages/main/mainComponents/MovieSwiper.tsx
+++ src/pages/main/mainComponents/MovieSwiper.tsx
@@ -1,25 +1,27 @@
+import ArrowBackIosNewRoundedIcon from '@mui/icons-material/ArrowBackIosNewRounded';
+import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
+import { Box, IconButton } from '@mui/material';
 import { useRef } from 'react';
 import { Swiper as SwiperType } from "swiper";
+import { Navigation } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.css';
 import type { MovieCardInfo } from '../../../_interfaces/interfaces';
-import MovieCardHorizontal from './MovieCardHorizontal';
-import { Box, IconButton } from '@mui/material';
-import ArrowBackIosNewRoundedIcon from '@mui/icons-material/ArrowBackIosNewRounded';
-import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
+import MovieCardBig from './MovieCardBig';
+import "./customSwiper.css";
 
 const sxBase = {
   position: "absolute",
   zIndex: 10,
   top: "50%",
   transform: "translateY(-50%)",
-  color: "oklch(0 0 0)",
+  color: "oklch(1 0 0)",
 }
 
 const PrevButton = ({ swiperRef }: { swiperRef: React.RefObject<SwiperType | null> }) => {
   return (
     <IconButton aria-label="previous"
-      sx={{...sxBase, left: 0}}
+      sx={{ ...sxBase, left: 0 }}
       onClick={() => swiperRef.current?.slidePrev()}>
 
       <ArrowBackIosNewRoundedIcon />
@@ -31,7 +33,7 @@ const PrevButton = ({ swiperRef }: { swiperRef: React.RefObject<SwiperType | nul
 const NextButton = ({ swiperRef }: { swiperRef: React.RefObject<SwiperType | null> }) => {
   return (
     <IconButton aria-label="previous"
-      sx={{...sxBase, right: 0}}
+      sx={{ ...sxBase, right: 0 }}
       onClick={() => swiperRef.current?.slideNext()}>
 
       <ArrowForwardIosRoundedIcon />
@@ -43,18 +45,20 @@ const NextButton = ({ swiperRef }: { swiperRef: React.RefObject<SwiperType | nul
 const MovieSwiper = ({ movieCardInfoArray }: { movieCardInfoArray: MovieCardInfo[] }) => {
   const swiperRef = useRef<SwiperType | null>(null);
 
-
   return (
-    <Box className='relative'>
+    <Box className='relative py-12'>
       <Swiper
-        spaceBetween={200}
-        slidesPerView={3}
+        modules={[Navigation]}
+        centeredSlides
+        slidesPerView="auto"
+        spaceBetween={100}
+        centerInsufficientSlides
         pagination={{ clickable: true }}
         onSwiper={(swiper: any) => { swiperRef.current = swiper }}>
 
         {movieCardInfoArray.map((movieCardInfo, index) => (
           <SwiperSlide>
-            <MovieCardHorizontal key={index} movieCardInfo={movieCardInfo} />
+            <MovieCardBig key={index} movieCardInfo={movieCardInfo} />
           </SwiperSlide>
         ))}
 
