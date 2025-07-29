e62fb2bc9330f1fefe1bc116bde5a0b347042beb
Tynahan <nusilite@gmail.com>
2025-07-29 21:29:13 +0900

    ✨ Feat(profile): Add profile icon when logged in

--- src/ProfileBox.tsx
+++ src/ProfileBox.tsx
@@ -2,9 +2,10 @@ import { Box } from '@mui/material'
 import useMovieStore from './_store/store'
 import { useNavigate } from 'react-router'
 import { useEffect } from 'react'
+import FaceIcon from '@mui/icons-material/Face';
 
 const ProfileBox = () => {
-  const navigate = useNavigate()
+  // const navigate = useNavigate()
   const user = useMovieStore((state) => state.user)
 
   useEffect(() => {
@@ -16,8 +17,8 @@ const ProfileBox = () => {
   }
 
   return (
-    <Box className="w-[40px] h-[40px] rounded-full bg-amber-300" onClick={handleClick}>
-      
+    <Box className="w-[54px] h-[54px] rounded-full bg-blue-400 flex justify-center items-center" onClick={handleClick}>
+      {user && <FaceIcon className="text-white" fontSize='large' />}
     </Box>
   )
 }
