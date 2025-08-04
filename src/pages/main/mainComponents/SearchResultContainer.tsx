1f3fc886be33406d142c824c26bfe3e1c7aa9726
Tynahan <nusilite@gmail.com>
2025-08-04 15:33:25 +0900

    🐛 Fix(scroll): Enable infinite scroll in search page

--- src/pages/main/mainComponents/SearchResultContainer.tsx
+++ src/pages/main/mainComponents/SearchResultContainer.tsx
@@ -3,11 +3,14 @@ import useMovieStore from '../../../_store/store'
 import { sortMovieDict } from '../../../_utils/utils'
 import CategoryTitle from './CategoryTitle'
 import MovieCardGrid from './MovieCardGrid'
+import { useBottomAdd } from '../../../_hooks/hooks'
 
 const SearchResultContainer = () => {
   const movieDict = useMovieStore((state) => state.movieDict)
 
   const movieArray = sortMovieDict(movieDict)
+
+  const { bottomRef } = useBottomAdd()
 
   return (
     <Box className="h-full overflow-y-scroll overflow-x-hidden">
@@ -15,6 +18,7 @@ const SearchResultContainer = () => {
       <CategoryTitle text='영화 검색 결과' />
       <MovieCardGrid movieArray={movieArray} />
 
+      <Box ref={bottomRef} />
     </Box>
   )
 }
