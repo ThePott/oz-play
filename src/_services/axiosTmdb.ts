dc7ba040a7c3085b360eabb3fa55bb6aef04d456
Tynahan <nusilite@gmail.com>
2025-08-05 15:13:39 +0900

    🐛 Fix(url): Fix api url with proper endpoint

--- src/_services/axiosTmdb.ts
+++ src/_services/axiosTmdb.ts
@@ -15,7 +15,6 @@ const axiosTmdb = axios.create({
     headers: { "Content-Type": "text/plain" }
 })
 axiosTmdb.interceptors.response.use(handleSuccess, handleFailure)
-// ---- !!!!! fill in here !!!! ----
-axiosTmdb.defaults.baseURL = process.env.NODE_ENV === 'development' ? "http://localhost:3456/tmdb" : "https://oz-play-api-3dc09dc1af38.herokuapp.com"
+axiosTmdb.defaults.baseURL = process.env.NODE_ENV === 'development' ? "http://localhost:3456/tmdb" : "https://oz-play-api-3dc09dc1af38.herokuapp.com/tmdb"
 
 export { axiosTmdb }
